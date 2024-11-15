50854854e5209dad3a31cfa3195b80915d7640c7
sarantg-vlt <saran@vulturelines.com>
2024-11-15 13:34:16 +0530

    Update Login.jsx

--- src/Auth/components/auth/Login.jsx
+++ src/Auth/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import assets from "../assets/assets";
 import { auth } from "../../firebase/firebaseConfig";
 import { signInWithEmailAndPassword } from "@firebase/auth";
@@ -6,7 +6,8 @@ import { toast } from "react-toastify";
 import { isStrongPassword, isValidEmail } from "../../utils/validityCheck";
 import { googlePopup } from "../../firebase/auth_google_popup";
 import { signinMicrosoft } from "../../firebase/auth_microsoft_execute";
-import { handleLinkedIn } from "../../firebase/auth_linkedIn_execute";
+// import { handleLinkedIn } from "../../firebase/auth_linkedIn_execute";
+import { signinLinkedIn, handleLinkedInCallback } from "../../firebase/auth_linkedIn_execute";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { check, loginCheck } from "../../../api/baseapi";
@@ -27,6 +28,25 @@ const Login = ({ toggleSlide }) => {
     if (!valid && error[type] === false) setError({ ...error, [type]: true });
   };
 
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    if (localStorage.getItem("isloggedin") === "true") {
+      if (localStorage.getItem("elacomplete") == "false") {
+        navigate("../quick-assessment");
+      } else {
+        if (Courseid) {
+          navigate("../home/courseDetails/" + Courseid);
+        } else {
+          navigate("../home");
+        }
+      }
+    }
+  }, []);
+
   const handleLogin = async () => {
     const newError = {};
     if (!isValidEmail(form.email)) {
@@ -39,9 +59,10 @@ const Login = ({ toggleSlide }) => {
       setError(newError);
       return;
     }
-
     try {
-      const res = await check({ email: form?.email });
+      const res = await check({
+        email: form?.email,
+      });
       if (res.status === 200) {
         try {
           const res = await loginCheck({
@@ -49,8 +70,11 @@ const Login = ({ toggleSlide }) => {
             password: form?.password,
           });
           toast.success("Login Successful");
-          localStorage.setItem("userDataUpdated", JSON.stringify(res.data.user));
-
+          console.log(res.data);
+          localStorage.setItem(
+            "userDataUpdated",
+            JSON.stringify(res.data.user)
+          );
           if (res.data.user.type == "user") {
             localStorage.setItem("isloggedin", true);
             localStorage.setItem("userid", res.data.user._id);
@@ -58,18 +82,23 @@ const Login = ({ toggleSlide }) => {
             localStorage.setItem("email", res.data.user.email);
             localStorage.setItem("linkedin", res.data.user.linkedin);
             localStorage.setItem("elacomplete", res.data.user.elaComplete);
-            setTimeout(() => {
-              if (res.data.user.elaComplete == false) {
+            if (res.data.user.elaComplete == false) {
+              setTimeout(() => {
                 navigate("../quick-assessment");
-              } else if (Courseid) {
-                navigate("../home/courseDetails/" + Courseid);
-              } else {
-                navigate("../home");
-              }
-            }, 5000);
+              }, 5000);
+            } else {
+              setTimeout(() => {
+                if (Courseid) {
+                  navigate("../home/courseDetails/" + Courseid);
+                } else {
+                  navigate("../home");
+                }
+              }, 5000);
+            }
           } else {
             toast.success("not a user?");
           }
+          console.log("User data stored in localStorage");
         } catch (error) {
           console.log("Invalid Password", error);
           toast.error("Invalid Password");
@@ -90,14 +119,36 @@ const Login = ({ toggleSlide }) => {
     let res;
     if (type === "google") {
       res = await googlePopup(navigate, Courseid);
-      setTimeout(() => {
-        if (Courseid) navigate("../home/courseDetails/" + Courseid);
-        else navigate("../home");
-      }, 5000);
+      if (res == "home") {
+        setTimeout(() => {
+          if (Courseid) {
+            navigate("../home/courseDetails/" + Courseid);
+          } else {
+            navigate("../home");
+          }
+        }, 5000);
+      }
+      if (res == "quick-assessment") {
+        setTimeout(() => {
+          navigate("../quick-assessment");
+        }, 5000);
+      }
     }
-    if (type === "github") handleLinkedIn();
-    if (type === "microsoft") res = await signinMicrosoft();
-  };
+    if (type === "linkedin") {
+      signinLinkedIn();
+    }
+     if (type === "microsoft") {
+    res = await signinMicrosoft();
+
+    if (res === "home") {
+      navigate("../home/courseDetails/" + Courseid);
+    } else if (res === "quick-assessment") {
+      navigate("../quick-assessment");
+    } else {
+      console.error("Error signing in with Microsoft.");
+    }
+  }
+};
 
   return (
     <div className="login-container">
@@ -116,12 +167,20 @@ const Login = ({ toggleSlide }) => {
               placeholder="Email or username"
               style={{ borderColor: error.email ? "red" : "#C7C7C7" }}
               onChange={(e) =>
-                handleValueChange("email", e.target.value, isValidEmail(e.target.value))
+                handleValueChange(
+                  "email",
+                  e.target.value,
+                  isValidEmail(e.target.value)
+                )
               }
               className="input"
             />
             {error.email && <p className="input-error">Enter valid Email</p>}
-            <img src={assets.Images.mail_icon} alt="mail-icon" className="icon" />
+            <img
+              src={assets.Images.mail_icon}
+              alt="mail-icon"
+              className="icon"
+            />
           </div>
           <div className="input-container">
             <input
@@ -129,14 +188,20 @@ const Login = ({ toggleSlide }) => {
               placeholder="Password"
               style={{ borderColor: error.password ? "red" : "#C7C7C7" }}
               onChange={(e) =>
-                handleValueChange("password", e.target.value, isStrongPassword(e.target.value))
+                handleValueChange(
+                  "password",
+                  e.target.value,
+                  isStrongPassword(e.target.value)
+                )
               }
               className="input"
             />
-            {error.password && <p className="input-error">Enter valid password</p>}
+            {error.password && (
+              <p className="input-error">Enter valid password</p>
+            )}
             <img
-              src={showPassword ? assets.Images.Eye : assets.Images.Eye_Closed_Icon}
-              alt="eye-icon"
+              src={assets.Images.Lock_Vector}
+              alt="mail-icon"
               className="icon"
               onClick={() => setShowPassword(!showPassword)}
             />
@@ -144,9 +209,14 @@ const Login = ({ toggleSlide }) => {
           <div className="options-container">
             <label className="checkbox-label">
               <input type="checkbox" className="checkbox" />
-              <p style={{ margin: "0px" }} className="checkbox-text">Remember me</p>
+              <p style={{ margin: "0px" }} className="checkbox-text">
+                Remember me
+              </p>
             </label>
-            <p onClick={() => toggleSlide("forgot-password")} className="forgot-password-link">
+            <p
+              onClick={() => toggleSlide("forgot-password")}
+              className="forgot-password-link"
+            >
               Forgot Password?
             </p>
           </div>
@@ -154,27 +224,59 @@ const Login = ({ toggleSlide }) => {
             <p style={{ margin: "0px" }}>Login</p>
           </div>
         </form>
-        {/* <div className="divider-container">
+        <div className="divider-container">
           <div className="divider"></div>
           <p className="divider-text">or</p>
           <div className="divider"></div>
-        </div> */}
+        </div>
         <div className="social-login-container">
-          <div className="social-login-button" onClick={() => handleSocialLogin("google")}>
-            <img src={assets.Images.Google} alt="Google" className="social-login-icon" />
+          <div
+            className="social-login-button"
+            onClick={() => handleSocialLogin("google")}
+          >
+            <img
+              src={assets.Images.Google}
+              alt="Google"
+              className="social-login-icon"
+            />
           </div>
-          <div className="social-login-button" onClick={() => handleSocialLogin("microsoft")}>
-            <img src={assets.Images.Microsoft} alt="Microsoft" className="social-login-icon" />
+          <div
+            className="social-login-button"
+            onClick={() => handleSocialLogin("microsoft")}
+          >
+            <img
+              src={assets.Images.Microsoft}
+              alt="Microsoft"
+              className="social-login-icon"
+            />
           </div>
-          <div className="social-login-button" onClick={() => handleSocialLogin("github")}>
-            <img src={assets.Images.LinkedIn} alt="LinkedIn" className="social-login-icon" />
+          {/* <div className="social-login-button">
+            <img
+              src={assets.Images.Apple}
+              alt="Apple"
+              className="social-login-icon"
+            />
+          </div> */}
+          <div
+            className="social-login-button"
+            onClick={() => handleSocialLogin("github")}
+          >
+            <img
+              src={assets.Images.LinkedIn}
+              alt="LinkedIn"
+              className="social-login-icon"
+            />
           </div>
         </div>
         <div className="signup-link-container">
           Don’t have an account?{" "}
-          <span style={{ margin: "0px" }} onClick={() => toggleSlide("signup")} className="signup-link">
+          <p
+            style={{ margin: "0px" }}
+            onClick={() => toggleSlide("signup")}
+            className="signup-link"
+          >
             Sign up.
-          </span>
+          </p>
         </div>
       </div>
     </div>
